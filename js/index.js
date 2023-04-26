5fbb6064661986932c6e61d34bb2ccc5cadefc74
ChristopherJamesLeo <aungaungoo.hotmail@gmail.com>
2023-04-26 18:29:26 +0630

    index finish

--- js/index.js
+++ js/index.js
@@ -12,6 +12,41 @@ $(document).ready(function(e){
 
         }
     })
+    // Start Our Team Section
+    $("#our_team_carousel").owlCarousel({
+        items : 1,
+        loop : true,
+        nav : true,
+        // dots : true,
+    })
+    // End Our Team Section
+
+    // Start Footer Instagram
+    // $('.insta_img').magnificPopup({
+    //     type: 'image',
+    //     mainClass: 'mfp-with-zoom', // this class is for CSS animation below
+    //     gallery : {
+    //         enable : true
+    //     },
+      
+    //     zoom: {
+    //       enabled: true, // By default it's false, so don't forget to enable it
+      
+    //       duration: 300, // duration of the effect, in milliseconds
+    //       easing: 'ease-in-out', // CSS transition easing function
+      
+    //       // The "opener" function should return the element from which popup will be zoomed in
+    //       // and to which popup will be scaled down
+    //       // By defailt it looks for an image tag:
+    //       opener: function(openerElement) {
+    //         // openerElement is the element on which popup was initialized, in this case its <a> tag
+    //         // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+    //         return openerElement.is('img') ? openerElement : openerElement.find('img');
+    //       }
+    //     }
+      
+    //   });
+    // End Footer Instagram
 })
 
 // Start Preloader
@@ -21,6 +56,8 @@ $(window).on("load",function(){
 })
 
 // End Preloader
+
+// Start Happy Client Counter
 const counterUp = window.counterUp.default
 
 const callback = entries => {
@@ -46,3 +83,4 @@ IO.observe( cap );
 IO.observe( shirt );
 IO.observe( bag );
 IO.observe( cafe );
+// end Happy Client Counter
